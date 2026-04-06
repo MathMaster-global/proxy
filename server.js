7fbb1d9a954c6bde455da9f263e0c36e48d09ddc
MathMaster-global <mathmasterofficialemail@gmail.com>
2026-04-06 17:30:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const { createServer } = require("http");
-const { WispServer } = require("@mercuryworkshop/wisp-js/server");
+const { server: wisp } = require("@mercuryworkshop/wisp-js/server");
 
 const app = express();
 const server = createServer(app);
 
 app.get("/", (req, res) => res.send("Wisp running"));
 
-const wisp = new WispServer();
-wisp.attach(server, "/wisp/");
+server.on("upgrade", (req, socket, head) => {
+    wisp.routeRequest(req, socket, head);
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
